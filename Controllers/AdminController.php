1eb28f537b58ee512d01d6479e39056d28c495e2
Oussema sferi <62106784+oussema-sferi@users.noreply.github.com>
2020-10-24 14:10:08 +0100

    routes protection ok

--- Controllers/AdminController.php
+++ Controllers/AdminController.php
@@ -1,3 +1,10 @@
 <?php
-
-require 'Views/AdminView.php';
+if(isset($_SESSION['user'])) {
+    if($_SESSION['user']->getRole() === 'admin') {
+        require 'Views/AdminView.php';
+    } else {
+        header('Location: router.php?controller=client');
+    }
+} else {
+    require 'Views/LoginView.php';
+}
--- Controllers/ClientController.php
+++ Controllers/ClientController.php
@@ -1,3 +1,11 @@
 <?php
+if(isset($_SESSION['user'])) {
+    if($_SESSION['user']->getRole() === 'client') {
+        require 'Views/ClientView.php';
+    } else {
+        header('Location: router.php?controller=admin');
+    }
+} else {
+    require 'Views/LoginView.php';
+}
 
-    require 'Views/ClientView.php';
